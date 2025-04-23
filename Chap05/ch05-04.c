a225a786d7727bca22ac414e739fcbb69c660b02
SEC <SEC@DESKTOP-AMV59HS>
2025-04-23 16:45:37 +0900

    6

--- Chap05/ch05-04.c
+++ Chap05/ch05-04.c
@@ -1,5 +1,5 @@
 //202511828 최주영
-//2025.4.20
+//2025.4.23
 //거스름돈의 금액을 입력받아 오만원, 만원, 천원, 백원, 십원짜리가 각각 몇 개 필요한지 구해서 출력하는 프로그램
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -7,6 +7,8 @@
 #include <stdio.h>
 
 void asgmt0503();
+
+void change(int S, int a, int b, int c, int d, int e, int f);
 
 int main()
 {
@@ -36,6 +38,14 @@ void asgmt0503()
 	//거스름돈 값을 출력한다
 	printf("거스름돈 (10원미만 절사): %d\n", S);
 
+	//함수 change를 호출한다
+	change(S, a, b, c, d, e, f);
+	
+	return;
+}
+
+void change(int S, int a, int b, int c, int d, int e, int f)
+{
 	//각각 몇 개 필요한지 계산한다
 	a = S / 50000;
 	S = S % 50000;
@@ -53,14 +63,14 @@ void asgmt0503()
 	S = S % 100;
 
 	f = S / 10;
-	
+
 	//결과를 출력한다
-	printf("  50000원  %d장\n",a);
-	printf("  10000원  %d장\n",b);
-	printf("   5000원  %d장\n",c);
-	printf("   1000원  %d장\n",d);
-	printf("    100원  %d개\n",e);
-	printf("     10원  %d개\n",f);
+	printf("  50000원  %d장\n", a);
+	printf("  10000원  %d장\n", b);
+	printf("   5000원  %d장\n", c);
+	printf("   1000원  %d장\n", d);
+	printf("    100원  %d개\n", e);
+	printf("     10원  %d개\n", f);
 
 	return;
 }
