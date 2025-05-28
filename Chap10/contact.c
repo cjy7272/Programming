b77efcb2766ed0de7e2332012a6f148a0a66be70
user <user@DESKTOP-VQEMIG3>
2025-05-28 16:39:46 +0900

    .

---
+++ Chap10/contact.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "contact.h"
+#include <string.h>
+
+//isEqualContact()
+//반환값: 만약 같으면 1 , 그렇지 않으면 0을 반환
+//입력: 비교할 두 Contact값
+
+int isEqualContact(Contact a, Contact b)
+{
+	if (a.year == b.year && a.gender == b.gender &&
+		strcmp(a.name, b.name) == 0 &&
+		strcmp(a.phone, b.phone) == 0)
+	{
+		return 1;
+	}
+
+	return 0;
+}
+
+void printContact(Contact ct)
+{
+	//이름: 000
+	//전번: 0000000000
+	//성별: 남 혹 여
+	//연도: 000
+
+	printf("이름: %s \n", ct.name);
+	printf("전번: %s \n", ct.phone);
+	printf("성별: %s \n", ct.gender == MAN ? "남자" : "여자");
+	printf("연도: %d \n", ct.year);
+
+}
